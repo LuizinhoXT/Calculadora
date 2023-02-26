22bdb30811f455b84b18e16535a864db1645dc68
LuizinhoXT <luiz.farias.work@outlook.com>
2023-02-26 04:31:51 -0300

    primeira soma realizada

--- Calculadora/src/logica/Controle.java
+++ Calculadora/src/logica/Controle.java
@@ -1,6 +1,50 @@
 package logica;
 
-class memoria{
+import javax.swing.text.StyledEditorKit.BoldAction;
 
+public class Controle extends Funcoes{
+    int memoria;
+    int numero;
+    int resultado;
+    
+    boolean somar;
+    
+    boolean adicao;
+    
+    public void definirOperacao(String operacao){
+        this.numero = memoria;
+        if(operacao.equals("+")){
+            somar = true;
+            this.numero = memoria;        
+        }
+    }
+
+    public int executarOperaçao(){
+        if(somar == true){
+            resultado = adicao(numero, memoria);    
+            System.out.println("chegou");
+        }
+        
+        return resultado;
+    }
+    public int getMemoria() {
+        return memoria;
+    }
+    public void setMemoria(int valor) {
+        this.memoria = valor;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public int getResultado() {
+        return resultado;
+    }
+    
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
     
 }
